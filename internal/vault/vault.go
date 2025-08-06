fae9c2574d9c9bf73820e9e65012a3be5af03cbb
KaareSkytte <kaareskytte1431@gmail.com>
2025-08-06 13:49:42 +0200

    added Save & loadVault functions

--- internal/vault/vault.go
+++ internal/vault/vault.go
@@ -1,8 +1,12 @@
 package vault
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/kaareskytte/pass-vault/internal/crypto"
 )
 
 func NewVault() *Vault {
@@ -17,7 +21,7 @@ func NewVault() *Vault {
 
 func (v *Vault) AddEntry(name string, entry Entry) error {
 	if _, exists := v.Entries[name]; exists {
-		return fmt.Errorf("Entry already exists: %v")
+		return fmt.Errorf("Entry already exists")
 	}
 
 	entry.Created = time.Now()
@@ -28,9 +32,69 @@ func (v *Vault) AddEntry(name string, entry Entry) error {
 }
 
 func (v *Vault) Save(password, filename string) error {
+	if v.Salt == nil {
+		salt, err := crypto.GenerateSalt()
+		if err != nil {
+			return err
+		}
+		v.Salt = salt
+	}
 
+	encryptionKey := crypto.GenerateEncryptionKey(password, v.Salt)
+
+	vault, err := json.Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	encryptedVault, err := crypto.Encrypt(vault, encryptionKey)
+	if err != nil {
+		return err
+	}
+
+	encryptedVaultFile := EncryptedVault{
+		Version:       v.Version,
+		Salt:          v.Salt,
+		EncryptedData: encryptedVault,
+	}
+
+	fileData, err := json.Marshal(encryptedVaultFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filename, fileData, 0600)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func LoadVault(password, filename string) (*Vault, error) {
+	jsonData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 
+	var encryptedVaultFile EncryptedVault
+	err = json.Unmarshal(jsonData, &encryptedVaultFile)
+	if err != nil {
+		return nil, err
+	}
+
+	encryptionKey := crypto.GenerateEncryptionKey(password, encryptedVaultFile.Salt)
+
+	decryptedData, err := crypto.Decrypt(encryptedVaultFile.EncryptedData, encryptionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	var vault Vault
+	err = json.Unmarshal(decryptedData, &vault)
+	if err != nil {
+		return nil, err
+	}
+
+	return &vault, nil
 }
