ddc3681b8d33e858033e97297999757743a7c15d
KaareSkytte <kaareskytte1431@gmail.com>
2025-08-08 10:43:01 +0200

    Added get functionality

--- cmd/get.go
+++ cmd/get.go
@@ -5,7 +5,12 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"syscall"
 
+	"golang.org/x/term"
+
+	"github.com/kaareskytte/pass-vault/internal/vault"
 	"github.com/spf13/cobra"
 )
 
@@ -15,20 +20,60 @@ var getCmd = &cobra.Command{
 	Short: "Gets a password entry from the vault",
 
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		if _, err := os.Stat(vault.DefaultVaultFile); err != nil {
+			fmt.Println("Create vault before adding entries with: pass-vault init")
+			return
+		}
+
+		if len(args) < 1 {
+			fmt.Println("Must enter entry name: pass-vault get \"entry-name-here\"")
+			return
+		}
+
+		fmt.Print("Enter master password: ")
+		masterPassword, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil {
+			fmt.Println("Invalid password")
+			return
+		}
+		fmt.Println()
+
+		v, err := vault.LoadVault(string(masterPassword), vault.DefaultVaultFile)
+		if err != nil {
+			fmt.Printf("Couldn't load vault: %v", err)
+			return
+		}
+
+		entryName := args[0]
+
+		entry, exists := v.Entries[entryName]
+		if !exists {
+			fmt.Printf("Entry \"%s\" not found\n", entryName)
+			return
+		}
+
+		showPassword, _ := cmd.Flags().GetBool("show-password")
+
+		fmt.Printf("Entry: %s\n", entry.Name)
+		fmt.Printf("Username: %s\n", entry.Username)
+
+		if showPassword {
+			fmt.Printf("Password: %s\n", entry.Password)
+		} else {
+			fmt.Printf("Password: [hidden - use --show-password to reveal]\n")
+		}
+
+		if entry.URL != "" {
+			fmt.Printf("URL: %s\n", entry.URL)
+		}
+
+		if entry.Notes != "" {
+			fmt.Printf("Notes: %s\n", entry.Notes)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	getCmd.Flags().BoolP("show-password", "p", false, "Show the password in output")
 }
